9f34fa43197fbe9afaa25945573268905f17f8d3
josemanuel0228 <josemanuelperez02082004@gmail.com>
2023-09-11 12:45:22 -0500

    programa de calculadora

---
+++ calculadora.py
@@ -0,0 +1,38 @@
+'''
+Autor: José Manuel Gutiérrez Pérez
+Fecha 11 de septiembre
+Descricion: claculadora de suma
+'''
+
+a= input("Ingresa el primer valor ")
+b= input("Ingresa el segundo valor")
+print (type(a), type(a))
+print("Suma ", a + b)
+a= float (a)
+b= float (b)
+print(type(a), type(b))
+print ("Suma ", a + b)
+
+print (type(a), type(a))
+print("Resta", a - b)
+a= float(a)
+b= float (b)
+print(type(a), type(b))
+print ("Resta ", a - b)
+
+print (type(a), type(a))
+print("Multiplicacion ", a * b)
+a= float (a)
+b= float (b)
+print(type(a), type(b))
+print ("Multiplicacion ", a * b)
+
+print (type(a), type(a))
+print("Division", a / b)
+a= float (a)
+b= float (b)
+print(type(a), type(b))
+print ("Division", a / b)
+
+
+
