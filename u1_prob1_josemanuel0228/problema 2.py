30f3d09f6aed6e8233e747ffd8e48f4d3351b8e7
josemanuel0228 <142534719+josemanuel0228@users.noreply.github.com>
2023-09-25 12:44:27 -0600

    Add files via upload

---
+++ u1_prob1_josemanuel0228/problema 2.py
@@ -0,0 +1,9 @@
+'''
+Autor: Jose Manuel Gutiérrez Pérez 
+Descripcion: Uso de operadores relacionales
+fecha: 25 de eptiembre del 2023
+'''
+a= int(input("ingresa el numero de panes "))
+print ("precio de panes",3.49)
+print ("descuento de 60%",*0.6)
+print("precio total", a * 3.49 *0.6 )
