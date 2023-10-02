82768d02cc0b5b6c7a00a7a70cd20b9139816a84
josemanuel0228 <142534719+josemanuel0228@users.noreply.github.com>
2023-10-02 10:58:38 -0600

    Add files via upload

---
+++ modulo 2/laboratorio 2.2.1.11.py
@@ -0,0 +1,7 @@
+﻿'''
+nombre: José Manuel Gutiérrez Pérez
+fecha: 2 de octubre del 2023
+descripcion
+Laboratorio: 2.2.1.11
+'''
+print(" \"Estoy\"\n","\"\"aprendiendo\"\"\n","\"\"\"python\"\"\"\n" )
