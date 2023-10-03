89d203d894dd7c22280be61edd8d72dc12eb18d0
josemanuel0228 <142534719+josemanuel0228@users.noreply.github.com>
2023-10-03 00:15:23 -0600

    Add files via upload

---
+++ modulo 3/3.1.1.10.py
@@ -0,0 +1,17 @@
+'''
+Autor: Jose Manuel Gutiérrez Pérez 
+Descripcion: laboratorio 3.1.1.10
+fecha: 2 de octubre del 2023
+
+'''
+
+planta = input()
+if planta == " ESPATIFILIO":
+    print("Si, ¡El espatifilio! es la mejor planta de todas la plantas del mundo")
+elif planta == "espatifilio":
+    print ("No, ¡no quiere un gran ESPATIFILIO")
+else:
+    print ("¡ESPATIFILIO!, ¡No pelargonico!")
+    
+
+    
---
+++ modulo 3/3.6.1.9.py
@@ -0,0 +1,22 @@
+﻿'''
+Autor: Jose Manuel Gutiérrez Pérez 
+Descripcion: laboratorio 3.6.1.9
+fecha: 2 de octubre del 2023
+
+'''
+my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+
+
+# Lista temporal para almacenar elementos únicos
+lista_sin_repeticiones = []
+
+# Iterar a través de la lista original
+for elemento in my_list:
+    # Verificar si el elemento no está en la lista temporal
+    if elemento not in lista_sin_repeticiones:
+        # Si no está en la lista temporal, agrégalo
+        lista_sin_repeticiones.append(elemento)
+
+
+print("La lista con elementos únicos:")
+print(lista_sin_repeticiones)
