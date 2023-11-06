a49fc158820c90d0f41f95809b9db29b388923eb
josemanuel0228 <142534719+josemanuel0228@users.noreply.github.com>
2023-11-06 13:32:56 -0600

    Delete modulo 2 directory

--- modulo 2/2.2.1.11.py
+++
@@ -1,7 +0,0 @@
-﻿'''
-nombre: José Manuel Gutiérrez Pérez
-fecha: 2 de octubre del 2023
-descripcion
-Laboratorio: 2.2.1.11
-'''
-print(" \"Estoy\"\n","\"\"aprendiendo\"\"\n","\"\"\"python\"\"\"\n" )
