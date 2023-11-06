d87737085b86a77026057a81d7a3262d1aed953d
josemanuel0228 <142534719+josemanuel0228@users.noreply.github.com>
2023-11-06 13:34:05 -0600

    Delete saludar.py

--- saludar.py
+++
@@ -1,7 +0,0 @@
-'''
-Autor: José Manuel Gutiérrez Pérez
-Fecha 11 de septiembre 
-Descricion: Iniciando con  python
-'''
-name = input("ingresa tu  nombre ")
-print('Hola', name) # Imprime variables 
